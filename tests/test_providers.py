fdf6e5f4557d9572baf8b8aa35e984a57dfb0d48
NotPeopling2day <32708219+NotPeopling2day@users.noreply.github.com>
2022-08-01 11:45:11 -0400

    tests: add tests

---
+++ tests/test_providers.py
@@ -0,0 +1,23 @@
+import pytest
+from ape import networks
+
+from ape_chainstack.providers import Chainstack, ChainstackProviderError
+
+
+@pytest.mark.skip("Wait until CI env is setup")
+def test_provider_ropsten():
+    # NOTE: env running test must have a key set for the network
+    # EXAMPLE: CHAINSTACK_ROPSTEN_URL=https://<address here>.com/<key>
+    with networks.ethereum.ropsten.use_provider("chainstack") as provider:
+        assert isinstance(provider, Chainstack)
+        assert provider.get_balance("0x0000000000000000000000000000000000000000") > 0
+
+
+def test_when_no_api_key_raises_error():
+    with pytest.raises(ChainstackProviderError) as err:
+        with networks.ethereum.mainnet.use_provider("chainstack"):
+            pass
+
+    expected = "Missing environment variable 'CHAINSTACK_MAINNET_URL'"
+    assert expected in str(err.value)
+    
